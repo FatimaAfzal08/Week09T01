2284f1639332b4d7845ebe12ec91636bf9b73461
FatimaAfzal08 <158256205+FatimaAfzal08@users.noreply.github.com>
2024-02-28 11:47:21 +0300

    Update test_book_manager.py

--- test_book_manager.py
+++ test_book_manager.py
@@ -6,8 +6,8 @@ class TestBookManager(unittest.TestCase):
         self.manager = BookManager()
         # Add setup for test books here
                 # Add setup for test books here
-        self.book1 = {'isbn':0000,'title':'Book One', 'author':'Author A'}
-        self.book1 = {'isbn':0002,''title':'Book One', 'author':'Author A'}
+        self.book1 = {'isbn':'0000','title':'Book One', 'author':'Author A'}
+        self.book1 = {'isbn':'0002',''title':'Book One', 'author':'Author A'}
         
 
     # Implement test methods here
@@ -23,7 +23,7 @@ class TestBookManager(unittest.TestCase):
 
     def test_remove_nonexistent_book(self):
         self.manager.add_book(self.book1)
-        self.manager.remove_book(0003)
+        self.manager.remove_book('0003')
         self.assertEqual(self.manager.list_book(),[self.book1])
         
 
